5b65b4c0f5686e4edca5493e04f00bd3fd5ba027
achmadnaufalwj <achmad.naufal@novelship.com>
2026-07-15 09:01:43 +0700

    fix: staging area standardize logic

--- dbt/models/staging/stg_customers.sql
+++ dbt/models/staging/stg_customers.sql
@@ -1,5 +1,5 @@
 SELECT
-    distinct
+    customer_id::string as customer_id,
     customer_unique_id::string as customer_unique_id,
     customer_zip_code_prefix::bigint as zip_code_prefix,
     customer_city::string as city,
